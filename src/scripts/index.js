5770d3087c972229eaa36ba7f519fdf0e1833e6e
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-01-28 20:34:47 -0300

    feat: adição das função para criptografar e descriptografar o texto

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -33,3 +33,57 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 toggle.addEventListener("click", switchTheme);
+
+// Criptografar e Descriptografar
+const botaoCriptografar = document.querySelector("#btn-criptografar");
+const botaoDescriptografar = document.querySelector("#btn-descriptografar");
+
+const arrayVogais = [
+    {
+        vogal: "a",
+        replace: "ai"
+    },
+    {
+        vogal: "e",
+        replace: "enter"
+    },
+    {
+        vogal: "i",
+        replace: "imes"
+    },
+    {
+        vogal: "o",
+        replace: "ober"
+    },
+    {
+        vogal: "u",
+        replace: "ufat"
+    },
+];
+
+function alteraTexto(parametro_de, parametro_para) {
+    let texto = document.querySelector("#area-do-texto").value;
+    const verificaMinusculas = /^[a-z]+$/;
+
+    if(texto) {
+        if(verificaMinusculas.test(texto)) {
+            const vogais = arrayVogais.filter((vogal) => {
+                return texto.includes(vogal[parametro_de]);
+            }) // A constante "vogais" gera um array de objetos
+            for (let i = 0; i < vogais.length; i++) {
+                const elemento = vogais[i][parametro_de];
+                const regex = new RegExp(elemento, "g");
+                texto = texto.replace(regex, vogais[i][parametro_para]);
+            }
+            // MOSTRAR RESULTADO NA TELA
+            console.log(texto);
+        } else console.log("texto inválido"); // MOSTRAR AVISO NA TELA QUE O TEXTO POSSUI CARACTERES INVÁLIDOS
+    } else console.log("campo de texto vazio!"); // MOSTRAR QUE A CAIXA DE TEXTO ESTÁ VAZIA
+}
+
+botaoCriptografar.addEventListener("click", () => {
+    alteraTexto("vogal", "replace");
+});
+botaoDescriptografar.addEventListener("click", () => {
+    alteraTexto("replace", "vogal");
+});
